5b37b13f454dc5dc8b42c8c731b2d47ecd5f66c9
gordonmaloney <gordonmaloney@gmail.com>
2023-07-21 15:53:18 +0100

    transitions, fixed button gloop spacing

---
+++ src/pages/Home/BikerackBlob.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import { Blob } from "../../components/Blob";
+import { Avatar, AvatarGroup, Button, Grid } from "@mui/material";
+import anime from "animejs/lib/anime.es.js";
+import { useNavigate } from "react-router-dom";
+import Tooltip from "@mui/material/Tooltip";
+
+const ButtonPoints = [
+  "M 389 246 C 419 217 435 231 453 259 C 419 259 442 273 427 291 C 393 284 425 273 409 305 C 379 290 386 280 386 305 C 352 287 386 288 366 285 C 361 264 352 288 381 260 C 355 246 381 232 389 247 Z",
+  "M 346 175 C 333 41 373 71 431 169 C 485 246 543 160 569 208 C 589 263 535 280 548 351 C 568 415 477 442 432 394 C 384 347 330 456 309 382 C 291 323 181 361 178 321 C 175 276 368 246 346 175 Z",
+  "M 0 0 C 183 1 358 0 552 0 C 687 0 799 3 799 0 C 800 268 800 321 800 600 C 591 596 501 599 417 600 C 292 595 179 599 0 600 C 4 473 4 443 0 365 C 6 256 -1 183 0 0 Z",
+];
+
+export const BikerackBlob = ({ name, bikers, id, setFade }) => {
+  const navigate = useNavigate();
+
+  const [play, setPlay] = useState(false);
+
+  anime({
+    targets: `.btnMorph${id}`,
+    d: [
+      {
+        value: ButtonPoints[1],
+      },
+      {
+        value: ButtonPoints[2],
+      },
+    ],
+    easing: "spring",
+    duration: 1000,
+    loop: false,
+    autoplay: play,
+  });
+
+  const openBr = (name) => {
+    setPlay(true);
+    setFade(true);
+    setTimeout(() => {
+      navigate("../bikerack/" + name);
+    }, 1000);
+  };
+
+  return (
+    <>
+      <div
+        onClick={() => openBr(name)}
+        style={{
+          marginTop: "-20px",
+          zIndex: "10",
+          cursor: "pointer",
+          position: "relative",
+          display: "inline-flex",
+          width: "220px",
+          height: "200px",
+        }}
+      >
+        <Blob colour={"white"} radius={90} squash={1.2} />
+
+        <h2
+          style={{
+            paddingTop: "50px",
+            color: "black",
+            zIndex: 5,
+            width: "220px",
+            textAlign: "center",
+          }}
+        >
+          {name}
+        </h2>
+
+        <div style={{ position: "absolute", bottom: "30px", left: "40px" }}>
+          <AvatarGroup
+            spacing="5"
+            max={4}
+            sx={{
+              "& .MuiAvatar-root": {
+                width: 30,
+                height: 30,
+                backgroundColor: "black",
+                fontSize: "small",
+                fontFamily: "'Passion One', cursive",
+                zIndex: 20,
+              },
+            }}
+          >
+            {bikers.map((biker, index) => {
+              if (index < 3)
+                return (
+                  <>
+                    <Tooltip title={biker}>
+                      <Avatar>
+                        {biker
+                          .toLowerCase()
+                          .split(" ")
+                          .map((word) => word.charAt(0))}
+                      </Avatar>
+                    </Tooltip>
+                  </>
+                );
+            })}
+            {bikers.length > 3 && (
+              <Tooltip
+                title={bikers.map(
+                  (biker, index) => index > 2 && <div>{biker}</div>
+                )}
+              >
+                <Avatar>+ {bikers.length - 3}</Avatar>
+              </Tooltip>
+            )}
+          </AvatarGroup>
+        </div>
+      </div>
+      <div style={{ position: "absolute" }}>
+        <svg
+          style={{
+            position: "relative",
+            left: "-50px",
+            top: "-250px",
+            transform: play ? `scale(10)` : "scale(0)",
+            height: "300px",
+            width: "300px",
+            transitionDuration: "1.5s",
+            transitionTimingFunction: "ease-out",
+            zIndex: 12,
+          }}
+          id={`btnMorph${id}`}
+          viewBox="0 0 800 600"
+          preserveAspectRatio="none"
+        >
+          <path class={`btnMorph${id}`} fill="white" d={ButtonPoints[0]} />
+        </svg>
+      </div>
+    </>
+  );
+};
+
+export default BikerackBlob;
--- src/pages/Home/LoggedIn.jsx
+++ src/pages/Home/LoggedIn.jsx
@@ -3,6 +3,34 @@ import { Blob } from "../../components/Blob";
 import { Avatar, AvatarGroup, Button, Grid } from "@mui/material";
 import anime from "animejs/lib/anime.es.js";
 import { useNavigate } from "react-router-dom";
+import { BikerackBlob } from "./BikerackBlob";
+
+const Racks = [
+  {
+    name: "drinking buddies",
+    bikers: ["Liam Johnson", "Sophia Anderson", "Noah Williams"],
+    id: 1,
+  },
+  {
+    name: "flatmates",
+    bikers: ["Ethan Thompson", "Ava Rodriguez", "Mason Lee"],
+    id: 2,
+  },
+  {
+    name: "the gang",
+    bikers: [
+      "Isabella Wright",
+      "Lucas Scott",
+      "Mia Phillips",
+      "Alexander Turner",
+      "Harper Ramirez",
+      "Benjamin Clark",
+      "Amelia Morris",
+      "Jameson Baker",
+    ],
+    id: 3,
+  },
+];
 
 const Points = [
   "M -1 0 C 216 0 216 0 216 0 C 479 0 479 0 479 0 C 563 0 563 0 563 0 C 795 0 795 0 795 0 L 800 600 L 800 600 L 0 600 L 0 0 ",
@@ -10,14 +38,8 @@ const Points = [
   "M -1 600 C 216 600 216 600 216 600 C 479 600 479 600 479 600 C 563 600 563 600 563 600 C 795 600 795 600 795 600 L 800 600 L 800 600 L 0 600 L 0 600 ",
 ];
 
-const ButtonPoints = [
-  "M 389 246 C 419 217 435 231 453 259 C 419 259 442 273 427 291 C 393 284 425 273 409 305 C 379 290 386 280 386 305 C 352 287 386 288 366 285 C 361 264 352 288 381 260 C 355 246 381 232 389 247 Z",
-  "M 346 175 C 333 41 373 71 431 169 C 485 246 543 160 569 208 C 589 263 535 280 548 351 C 568 415 477 442 432 394 C 384 347 330 456 309 382 C 291 323 181 361 178 321 C 175 276 368 246 346 175 Z",
-  "M 0 0 C 183 1 358 0 552 0 C 687 0 799 3 799 0 C 800 268 800 321 800 600 C 591 596 501 599 417 600 C 292 595 179 599 0 600 C 4 473 4 443 0 365 C 6 256 -1 183 0 0 Z",
-];
 
 const LoggedIn = () => {
-  const navigate = useNavigate();
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
@@ -33,11 +55,7 @@ const LoggedIn = () => {
     };
   }, []);
 
-  const [play, setPlay] = useState(false);
-  const openBr = (name) => {
-    setPlay(true);
-    //navigate("../bikerack/" + name);
-  };
+  const [fade, setFade] = useState(false);
 
   anime({
     targets: ".morph",
@@ -52,73 +70,8 @@ const LoggedIn = () => {
     easing: "spring",
     duration: 1000,
     loop: false,
-    autoplay: !play,
+    autoplay: !fade,
   });
-  anime({
-    targets: ".btnMorph",
-    d: [
-      {
-        value: ButtonPoints[1],
-      },
-      {
-        value: ButtonPoints[2],
-      },
-    ],
-    easing: "spring",
-    duration: 1000,
-    loop: false,
-    autoplay: play,
-  });
-
-  const Rack = ({ name, bikers }) => {
-    return (
-      <div
-        onClick={() => openBr(name)}
-        style={{
-          zIndex: "10",
-          cursor: "pointer",
-          position: "relative",
-          display: "inline-flex",
-          width: "220px",
-          height: "180px",
-        }}
-      >
-        <Blob colour={"white"} radius={90} squash={1.2} />
-
-        <h2
-          style={{
-            paddingTop: "50px",
-            color: "black",
-            zIndex: 5,
-            width: "220px",
-            textAlign: "center",
-          }}
-        >
-          {name}
-        </h2>
-
-        <div style={{ position: "absolute", bottom: "10px", left: "40px" }}>
-          <AvatarGroup
-            spacing="5"
-            max={4}
-            sx={{
-              "& .MuiAvatar-root": {
-                width: 30,
-                height: 30,
-                backgroundColor: "black",
-                fontSize: "small",
-                fontFamily: "'Passion One', cursive",
-              },
-            }}
-          >
-            {bikers.map((biker) => (
-              <Avatar>{biker}</Avatar>
-            ))}
-          </AvatarGroup>
-        </div>
-      </div>
-    );
-  };
 
   return (
     <div
@@ -126,7 +79,10 @@ const LoggedIn = () => {
         width: "100vw",
         maxWidth: "100vw",
         minHeight: "100vh",
-        overflowX: "hidden",
+        overflow: "hidden",
+        opacity: fade ? 0 : 1,
+        transition: "1s",
+        transitionTimingFunction: "ease-in",
       }}
     >
       <svg
@@ -169,8 +125,7 @@ const LoggedIn = () => {
       <div
         style={{
           position: "relative",
-          display: "inline-flex",
-          alignItems: "center",
+
           color: "white",
         }}
       >
@@ -186,7 +141,7 @@ const LoggedIn = () => {
             })`,
           }}
         >
-          <Blob colour={"black"} radius={100} squash={1} />
+          <Blob colour={"black"} radius={100} squash={1.1} />
         </div>
 
         <h2
@@ -196,7 +151,7 @@ const LoggedIn = () => {
             zIndex: 2,
             fontSize: "2em",
             left: "50px",
-            marginTop: "-370px",
+            marginTop: "-200px",
           }}
         >
           your bikeracks
@@ -205,37 +160,26 @@ const LoggedIn = () => {
         <Grid
           container
           style={{
+            justifyContent: "space-around",
             maxWidth: "100vw",
             position: "absolute",
-            marginTop: "-150px",
+            marginTop: "-170px",
           }}
         >
-          <Grid item>
-            <Rack name="drinking buddies" bikers={["gm", "mv", "mw"]} />
-            <svg
-              style={{
-                position: "absolute",
-                marginLeft: "-270px",
-                marginTop: "-170px",
-                transform: play && `scale(1.5)`,
-                zIndex: play && 12
-              }}
-              id="btnMorph"
-              height="100vh"
-              width="100vw"
-              viewBox="0 0 800 600"
-              preserveAspectRatio="none"
-            >
-              <path class="btnMorph" fill="white" d={ButtonPoints[0]} />
-            </svg>
-          </Grid>
-          <Grid item>
-            <Rack name="flatmates" bikers={["wa", "vd", "nj"]} />
-          </Grid>
-
-          <Grid item>
-            <Rack name="the gang" bikers={["on", "sc", "ma", "vd", "nj"]} />
-          </Grid>
+          {Racks.map((rack) => {
+            return (
+              <Grid item>
+                <center>
+                  <BikerackBlob
+                    name={rack.name}
+                    bikers={rack.bikers}
+                    id={rack.id}
+                    setFade={setFade}
+                  />
+                </center>
+              </Grid>
+            );
+          })}
         </Grid>
       </div>
     </div>
